5710d07211f377de5fdaa26bcf31dcebce740e0c
Robert Hintz <rhintz42@stanford.edu>
2013-05-23 11:41:20 -0700

    Adding initial files

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RailsTestDebugging::Application.config.secret_token = 'befeb0333ab0fc8dddadbc7bbdad966639003442ba02a0ca66b0f5660e1ccc1ae1604c2d3e3bb0a5180f4ea0dd58a9459b05a3cefc1645ef1fcc0d01766eefa7'
